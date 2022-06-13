585bb383c1ec8f4eef0435018d8783d7a00ccb6d
Aleksey <82260790+Zont1k@users.noreply.github.com>
2022-06-13 22:39:44 +0200

    Update Boticord.Net.cs

--- Boticord.Net.cs
+++ Boticord.Net.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using SDC_Sharp.Types;
+using Boticord.Net.Types;
 
 namespace Boticord.Net
 {
